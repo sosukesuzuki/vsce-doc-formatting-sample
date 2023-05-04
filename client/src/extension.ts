63918be7d01108fe81f3e33e7f35c3cdd3a8dd5a
Sosuke Suzuki <sosuke.suzuki@dr-ubie.com>
2023-05-04 20:41:58 +0900

    Implement minimal client

---
+++ client/src/extension.ts
@@ -0,0 +1,39 @@
+import * as path from "node:path";
+import type { ExtensionContext } from "vscode";
+
+import {
+  LanguageClient,
+  LanguageClientOptions,
+  ServerOptions,
+  TransportKind,
+} from "vscode-languageclient/node";
+
+let client: LanguageClient;
+
+export function activate(context: ExtensionContext) {
+  const serverModule = context.asAbsolutePath(
+    path.join("server", "out", "server.js")
+  );
+  const serverOptions: ServerOptions = {
+    run: { module: serverModule, transport: TransportKind.ipc },
+    debug: { module: serverModule, transport: TransportKind.ipc },
+  };
+
+  const clientOptions: LanguageClientOptions = {};
+
+  client = new LanguageClient(
+    "vsceDocFormattingSample",
+    "Doc Formatting VSCode Extension Sample",
+    serverOptions,
+    clientOptions
+  );
+
+  client.start();
+}
+
+export function deactivate(): Thenable<void> | undefined {
+  if (!client) {
+    return undefined;
+  }
+  return client.stop();
+}
