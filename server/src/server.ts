b41a32023589363aa5bb89963ef3cb23c9b644b0
Sosuke Suzuki <sosuke.suzuki@dr-ubie.com>
2023-05-04 21:17:45 +0900

    Implement simple formatting logic

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,19 +1,43 @@
+import { TextDocument } from "vscode-languageserver-textdocument";
 import {
   InitializeResult,
   ProposedFeatures,
+  Range,
+  TextDocumentSyncKind,
+  TextDocuments,
+  TextEdit,
   createConnection,
 } from "vscode-languageserver/node";
 
 const connection = createConnection(ProposedFeatures.all);
 
+const documents: TextDocuments<TextDocument> = new TextDocuments(TextDocument);
+
 connection.onInitialize(() => {
   const result: InitializeResult = {
     capabilities: {
+      textDocumentSync: TextDocumentSyncKind.Incremental,
       documentFormattingProvider: true,
-      documentRangeFormattingProvider: true,
     },
   };
   return result;
 });
+
+connection.onDocumentFormatting(async (params) => {
+  const document = documents.get(params.textDocument.uri);
+  if (document === undefined) {
+    return [];
+  }
+  const text = document.getText();
+  let newText = "";
+  for (const char of text) {
+    newText += `${char} `;
+  }
+  const pos0 = document.positionAt(1);
+  const pos1 = document.positionAt(text.length - 1);
+  return [TextEdit.replace(Range.create(pos0, pos1), newText)];
+});
+
+documents.listen(connection);
 
 connection.listen();
